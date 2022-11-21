f1642d57e80138a9d10536f076436cba48ac83d0
Devendra Yadav <devendra.yadav@gmail.com>
2022-11-22 00:02:53 +0800

    Assignment 21 completed

--- test.js
+++ test.js
@@ -1,8 +1,24 @@
-console.log('start')
-Promise.resolve(1).then((res) => {
-	console.log(res)
-})
-Promise.resolve(2).then((res) => {
-	console.log(res)
-})
-console.log('end')
+
+
+async function doSomething(){
+	console.log("Do SOmething started");
+	setTimeout(()=>{
+		console.log("inside doSoething");
+	},5000);
+
+	console.log("Do something end");
+	
+}
+
+console.log("Start");
+let out = doSomething();
+
+out.then((abcd)=>{
+	console.log(abcd);
+}).catch(()=>{
+	console.log("Failures");
+});
+
+out.then
+
+console.log("END");
